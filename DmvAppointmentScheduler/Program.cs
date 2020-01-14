c764bc40061694dfe9a526a80485177c92555209
jaysynwill <jaysynwill@gmail.com>
2020-01-14 13:30:20 -0600

    Add files via upload

---
+++ DmvAppointmentScheduler/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DmvAppointmentScheduler
+{
+    class Program
+    {
+        public static Random random = new Random();
+        public static List<Appointment> appointmentList = new List<Appointment>();
+        static void Main(string[] args)
+        {
+            CustomerList customers = ReadCustomerData();
+            TellerList tellers = ReadTellerData();
+            Calculation(customers, tellers);
+            OutputTotalLengthToConsole();
+
+        }
+        private static CustomerList ReadCustomerData()
+        {
+            string fileName = "CustomerData.json";
+            string path = Path.Combine(Environment.CurrentDirectory, @"InputData\", fileName);
+            string jsonString = File.ReadAllText(path);
+            CustomerList customerData = JsonConvert.DeserializeObject<CustomerList>(jsonString);
+            return customerData;
+
+        }
+        private static TellerList ReadTellerData()
+        {
+            string fileName = "TellerData.json";
+            string path = Path.Combine(Environment.CurrentDirectory, @"InputData\", fileName);
+            string jsonString = File.ReadAllText(path);
+            TellerList tellerData = JsonConvert.DeserializeObject<TellerList>(jsonString);
+            return tellerData;
+
+        }
+        static void Calculation(CustomerList customers, TellerList tellers)
+        {
+            List<Teller> specialtyOne = new List<Teller>();
+            List<Teller> specialtyZero = new List<Teller>();
+            List<Teller> specialtyTwo = new List<Teller>();
+            List<Teller> specialtyThree = new List<Teller>();
+            foreach (Teller teller in tellers.Teller)
+            {
+                if (teller.specialtyType == "0")
+                {
+                    specialtyZero.Add(teller);
+                }
+                if (teller.specialtyType == "1")
+                {
+                    specialtyOne.Add(teller);
+                }
+                if (teller.specialtyType == "2")
+                {
+                    specialtyTwo.Add(teller);
+                }
+                if (teller.specialtyType == "3")
+                {
+                    specialtyThree.Add(teller);
+                }
+            }
+            // Your code goes here .....
+            // Re-write this method to be more efficient instead of a random assignment
+            foreach(Customer customer in customers.Customer)
+            {
+                int i = 0;
+                int j = 0;
+                int k = 0;
+                int l = 0;
+                if(customer.type== "0")
+                {
+                    var appointmentZero = new Appointment(customer, specialtyZero[i]);
+                    appointmentList.Add(appointmentZero);
+                    i++;
+                    if (i > specialtyZero.Count)
+                    {
+                        i = 0;
+                    }
+                }
+                if (customer.type == "1")
+                {
+                    var appointmentOne = new Appointment(customer, specialtyOne[j]);
+                    appointmentList.Add(appointmentOne);
+                    j++;
+                    if (j > specialtyOne.Count)
+                    {
+                        j = 0;
+                    }
+                }
+                if (customer.type == "2")
+                {
+                    var appointmentTwo = new Appointment(customer, specialtyTwo[k]);
+                    appointmentList.Add(appointmentTwo);
+                    k++;
+                    if (k > specialtyTwo.Count)
+                    {
+                        k = 0;
+                    }
+                }
+                if (customer.type == "3")
+                {
+                    var appointmentThree = new Appointment(customer, specialtyThree[l]);
+                    appointmentList.Add(appointmentThree);
+                    l++;
+                    if (l > specialtyThree.Count)
+                    {
+                        l = 0;
+                    }
+                }            
+               
+            }
+        }
+        static void OutputTotalLengthToConsole()
+        {
+            var tellerAppointments =
+                from appointment in appointmentList
+                group appointment by appointment.teller into tellerGroup
+                select new
+                {
+                    teller = tellerGroup.Key,
+                    totalDuration = tellerGroup.Sum(x => x.duration),
+                };
+            var max = tellerAppointments.OrderBy(i => i.totalDuration).LastOrDefault();
+            Console.WriteLine("Teller " + max.teller.id + " will work for " + max.totalDuration + " minutes!");
+        }
+
+    }
+}
